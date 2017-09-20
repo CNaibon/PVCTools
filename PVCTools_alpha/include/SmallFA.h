7590e2a134915752153d7c5da765cdbcead8bfa3
Liu JiaJun <CNaibon@users.noreply.github.com>
2017-09-20 20:24:54 +0800

    20170919
    结构调整

---
+++ PVCTools_alpha/include/SmallFA.h
@@ -0,0 +1,20 @@
+//
+// Created by liujiajun on 2017/4/21.
+//
+
+#ifndef PVCTOOLS_ALPHA_V2_0_SMALLFA_H
+#define PVCTOOLS_ALPHA_V2_0_SMALLFA_H
+
+#include <iostream>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+#include <omp.h>
+#include <time.h>
+#include "main.h"
+
+int SmallFA_HeaderModify(char *file_name, const char *chr_name);
+int SmallFA(int argc, char *argv[]);
+
+#endif //PVCTOOLS_ALPHA_V2_0_SMALLFA_H
